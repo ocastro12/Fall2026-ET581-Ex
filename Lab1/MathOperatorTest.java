dea4375de2b2779e0b97d15e06f1f4bab3d8fe90
ocastro12 <oc4126oc@gmail.com>
2026-09-03 23:45:41 +0000

    Lab1 second commit

---
+++ Lab1/MathOperatorTest.java
@@ -0,0 +1,11 @@
+public class MathOperatorTest {
+    public static void main(String[] args){
+        int x;
+        x = 0;
+        int y = 1;
+        System.out.println("x = " + x);
+        System.out.println("y = "+ y);
+        System.out.println("7/2 = " + 7/2);
+        System.out.println("7%2 = " + 7%2);
+    }
+}
